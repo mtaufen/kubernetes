f01292c8d1285b630ecd14ec3dad531d81afbfa0
Michael Taufen <mtaufen@google.com>
2018-03-16 21:32:37 -0700

    disable flag parsing

--- cmd/testcmd/testcmd.go
+++ cmd/testcmd/testcmd.go
@@ -37,26 +37,49 @@ func init() {
 
 func NewLocalFlagSet() *pflag.FlagSet {
 	fs := pflag.NewFlagSet(use, pflag.ContinueOnError)
-	fs.StringVar(&localFlagTarget, "test", localFlagTarget, "locally-registered flag")
+	fs.StringVar(&localFlagTarget, "local-flag", localFlagTarget, "locally-registered flag")
 	return fs
 }
 
 func NewTestCmd() *cobra.Command {
+	localFlagSet := NewLocalFlagSet()
 	cmd := &cobra.Command{
-		Use: use,
+		Use:                use,
+		DisableFlagParsing: true,
 		Run: func(cmd *cobra.Command, args []string) {
+			// parse our local flag set
+			if err := localFlagSet.Parse(args); err != nil {
+				cmd.Usage()
+				fatal(err)
+			}
+			// --help
+			help, err := localFlagSet.GetBool("help")
+			if err != nil {
+				fatal(fmt.Errorf(`"help" flag is non-bool, programmer error, please correct`))
+			}
+			if help {
+				cmd.Help()
+				return
+			}
+			// print the flag values
 			fmt.Printf("globalFlagTarget: %q\n", globalFlagTarget)
 			fmt.Printf("localFlagTarget: %q\n", localFlagTarget)
 		},
 	}
-	cmd.Flags().AddFlagSet(NewLocalFlagSet())
+	localFlagSet.BoolP("help", "h", false, fmt.Sprintf("help for %s", cmd.Name()))
+	// Cobra still needs to be aware of our flag set to generate usage and help text
+	cmd.Flags().AddFlagSet(localFlagSet)
 	return cmd
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	os.Exit(1)
 }
 
 func main() {
 	cmd := NewTestCmd()
 	if err := cmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+		fatal(err)
 	}
 }
