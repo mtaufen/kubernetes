45111129c0e245b677304c145872d3ce14fc245a
Michael Taufen <mtaufen@google.com>
2019-01-08 10:02:32 -0800

    other

--- pkg/componentflags/dummy.go
+++ pkg/componentflags/dummy.go
@@ -3,44 +3,117 @@ package dummy
 // Similar to "direct," but register flags against an internal dummy config, and
 // return a function that can do the application.
 
-// Config
-// TODO: Add some more advanced types here
-type Config struct {
-	A int32
-	B int32
-}
-
 // Now that flags are registered directly against a Config, we don't
 // need to duplicate the fields in the Flags struct.
 
-func AddFlags(fs *pflag.FlagSet) func(c *Config) {
-	// Assumes tmp is correctly defaulted already
-	tmp := &Config{}
-	fns := map[string]func(tmp, c *Config){}
+// The common cases should be easy, but the uncommon cases should still be possible.
 
-	fs.Int32Var(&tmp.A, "flag-A", tmp.A, "A")
-	fns["flag-A"] = func(tmp, c *Config) {
-		c.B = tmp.B
-	}
+// Can we abstract away apply behavior as well?
+// Since it's a Config -> Config function, field names
+// match up.
+// What if the flag registration function could accept merge behavior?
+// Is there a way to do this without losing type safety?
 
-	fs.Int32Var(&tmp.B, "flag-B", tmp.B, "B")
-	fns["flag-B"] = func(tmp, c *Config) {
-		c.C = tmp.C
-	}
+// Since we always make the default the same as the current value of the field, we can
+// omit that too...
 
-	return mkApply(fs, tmp, fns)
+// TODO(mtaufen): probably keep usage string at the end of the arguments
+// For example, could we pass a field path in here and reflect on it to get the value and a pointer?
+// TODO: How can we get rid of the need for users to typecast in the apply func that they write? (HINT HINT MACROS OR GENERICS WOULD BE NICE!)
+
+// Config is the internal representation of a component config
+type Config struct {
+	A int32
+	B []string
 }
 
-func mkApply(fs *pflag.FlagSet, tmp *Config, applyFuncs map[string]func(tmp, c *Config)) {
-	return func(c *Config) {
-		for name, apply := range applyFuncs {
-			flag := fs.Lookup(name)
-			if flag == nil {
-				panic("Missing flag %s from flagset during apply!", name)
-			}
-			if flag.Changed {
-				apply(f, c)
-			}
+// NewConfig returns a Config with default values set
+func NewConfig() *Config {
+	c := &Config{}
+	Default(c)
+	return c
+}
+
+// Default applies defautls to a Config, if values are not already set
+func Default(c *Config) {
+	if c.A == 0 {
+		c.A = 1
+	}
+	if c.B == nil {
+		c.B = []string{"foo"}
+	}
+}
+
+// ConfigFlagSet will register an applyFunc every time a flag is registered, and
+// can apply all of the applyFuncs to a Config, using values parsed into the tmp Config.
+type ConfigFlagSet struct {
+	tmp        interface{}
+	fs         *pflag.FlagSet
+	applyFuncs map[string]func(c *Config)
+}
+
+// tmp should already contain the default values
+func NewConfigFlagSet(fs *pflag.FlagSet, tmp interface{}) *ConfigFlagSet {
+	return &ConfigFlagSet{tmp: tmp, fs: fs}
+}
+
+func (fs *ConfigFlagSet) Int32Var(name string, usage string, target func(c interface{}) *int32, merge func(tmp, c int32) int32) {
+	ptmp := target(fs.tmp)
+	fs.fs.Int32Var(ptmp, name, *ptmp, usage)
+	// This direct application may be a reasonable default
+	fs.applyFuncs[name] = func(c interface{}) {
+		pc := target(c)
+		if merge == nil {
+			*pc = *ptmp
+		} else {
+			*pc = merge(*ptmp, *pc)
 		}
 	}
+}
+
+func (fs *ConfigFlagSet) StringSliceVar(name string, usage string,
+	target func(c interface{}) *[]string, merge func(tmp, c []string) []string) {
+	ptmp := target(fs.tmp)
+	fs.fs.StringSliceVar(ptmp, name, *ptmp, usage)
+	// This direct application may be a reasonable default
+	fs.applyFuncs[name] = func(c interface{}) {
+		pc := target(c)
+		if merge == nil {
+			*pc = []string{}
+			for _, v := range *ptmp {
+				*pc = append(*pc, v)
+			}
+		} else {
+			// TODO(mtaufen): also consider copy before calling merge here, to avoid side-effects
+			*pc = merge(*ptmp, *pc)
+		}
+	}
+}
+
+func (fs *ConfigFlagSet) Apply(c interface{}) {
+	for name, apply := range applyFuncs {
+		if fs.fs.Changed(name) {
+			apply(c)
+		}
+	}
+}
+
+func AddConfigFlags(fs *pflag.FlagSet) func(c interface{}) {
+	tmp = NewConfig()
+	cfs := NewConfigFlagSet(fs)
+
+	cfs.Int32Var("a-flag", "", func(c interface{}) *int32 {
+		return &c.(*Config).A
+	}, nil)
+
+	cfs.StringSliceVar("b-flag", "", func(c interface{}) *[]string {
+		return &c.(*Config).B
+	}, func(a, b []string) []string {
+		s := []string{}
+		s = append(s, a...)
+		s = append(s, b...)
+		return s
+	})
+
+	return cfs.Apply
 }
